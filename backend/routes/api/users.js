e4d7a553005a82ec8b66c303c5413e8a560317d9
Joemanf <joemanf0@gmail.com>
2021-04-25 22:07:52 -0400

    Added signup functionality

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,4 +1,24 @@
-const express = require('express')
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
 const router = express.Router();
+
+// Sign up
+router.post(
+    '',
+    asyncHandler(async (req, res) => {
+        const { email, password, username } = req.body; // Grab the info from the req
+        const user = await User.signup({ email, username, password }); // Make yourself
+
+        await setTokenCookie(res, user);
+
+        return res.json({
+            user,
+        });
+    }),
+);
 
 module.exports = router;
