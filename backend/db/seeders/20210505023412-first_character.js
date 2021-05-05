f97d2398d3d67a48c3e001f8667dde77ac84f70f
Joemanf <joemanf0@gmail.com>
2021-05-04 22:36:08 -0400

    Added some seed data

---
+++ backend/db/seeders/20210505023412-first_character.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Characters', [
+      { name: 'Eugene', class: 'Explorer', userId: 1, strength: 8, dexterity: 7, constitution: 8, intelligence: 3, wisdom: 5, charisma: 7, avatar: 'https://cdn.discordapp.com/attachments/661999012873764935/839327174892060697/EUGENE_shorter_neck.png' }
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+    return queryInterface.bulkDelete('Characters', null, {});
+  }
+};
