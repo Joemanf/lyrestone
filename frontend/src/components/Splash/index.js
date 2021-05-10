5590912278a455f347d9a3f9561b5b6ff654f728
Joemanf <joemanf0@gmail.com>
2021-05-10 16:03:25 -0400

    Please make this work

--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavLink, Redirect } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
@@ -8,6 +8,22 @@ import logo from '../../images/lyrestone.png'
 
 function Splash() {
     const sessionUser = useSelector((state) => state.session.user); // Grabbing logged in user
+
+    useEffect(() => {
+        const text = document.querySelector('.logo_text')
+        const image = document.querySelector('.logo_container_inner');
+        const links = document.querySelector('.inner_links');
+        setTimeout(() => {
+            text.style.top = '0';
+            text.style.opacity = '1';
+        }, 1000)
+        setTimeout(() => {
+            image.style.left = '0';
+            image.style.opacity = '1';
+            links.style.bottom = '0';
+            links.style.opacity = '1';
+        }, 4000)
+    }, [])
 
     if (sessionUser) return <Redirect to="/home" />; // Change to history.push might fix the store
 
@@ -20,7 +36,7 @@ function Splash() {
                     </div>
                     <h1 className='logo_text'>Lyrestone</h1>
                 </div>
-                <div className='links'>
+                <div className='inner_links'>
                     <NavLink to='/login'>Log In</NavLink>
                     <NavLink to='/signup'>Sign Up</NavLink>
                 </div>
